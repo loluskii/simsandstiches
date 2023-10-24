103d329b7fa0764b19a96f16f7dea9ec0b10db5d
Lolu Adelore <adeloreisaac@gmail.com>
2023-10-24 14:00:02 +0100

    update poroduct model to take mass assignment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,19 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    public function orders(){
+    protected $fillable = ['sku', 'slug', 'name', 'description', 'category_id', 'price', 'additional_information'];
+    public function orders()
+    {
         return $this->hasMany(Order::class);
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function images(){
+    public function images()
+    {
         return $this->hasMany(Image::class);
     }
 
-    public function attributes(){
+    public function attributes()
+    {
         return $this->hasMany(ProductAttribute::class);
     }
 }
